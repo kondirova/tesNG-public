e4543e7456a1e0bdd414de3b3bfdadfeeff731d2
Sofia Kondirova <sofia.kondirova@jetbrains.com>
2024-02-22 15:33:48 +0100

    Initial commit

---
+++ src/test/java/TestCase.java
@@ -0,0 +1,9 @@
+import org.testng.*;
+import org.testng.annotations.Test;
+
+public class TestCase {
+    @Test
+    public void givenNumber_whenEven_thenTrue() {
+        Assert.assertEquals(4, 3);
+    }
+}
